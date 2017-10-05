973d4e24423b46cbba3f12b4e8fa61a866e6cae4
Peter <shadman48@gmail.com>
2017-10-05 14:36:23 -0700

    Finnaly have the client connecting to the server. Also added multi-threading to be able to talk and recive at the same time.

---
+++ client.java
@@ -0,0 +1,90 @@
+
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Random;
+
+public final class client {
+
+    public static void main(String[] args) throws Exception {
+        
+    	
+    	
+    	
+    	
+    	try (Socket socket = new Socket("18.221.102.182", 38001)) 
+        {
+			String username = "Shadman48";
+			//Send user name.
+			InputStream in = socket.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			OutputStream os = socket.getOutputStream();
+			PrintStream ps = new PrintStream(os, true, "UTF-8");
+			ps.println(username);
+			
+			
+		
+       	
+        	
+        	//Receive response from server.
+			
+			InputStreamReader isr = new InputStreamReader(in, "UTF-8");
+			BufferedReader brIn = new BufferedReader(new InputStreamReader(System.in));
+			String serverOutput = br.readLine();
+				
+			Runnable chatLog = () -> 
+	        {
+	            while (true) {	
+	            	if(serverOutput != null){
+						try {
+							if(!serverOutput.contains(username))
+							System.out.println(br.readLine());
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+	            	}
+	            }
+	        };
+	        
+	        
+	        
+	        Runnable clientSide = () -> 
+	        {
+	        	String userInput = "";
+	            while (true) {	
+	            	try {
+						userInput = brIn.readLine();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				    ps.println(userInput);
+	            }
+	        };
+	        
+	        
+	        
+	        
+	        Thread chatLogThread = new Thread(chatLog);
+	        chatLogThread.start();
+			
+	        Thread clientSideThread = new Thread(clientSide);
+	        clientSideThread.start();
+		 
+        }
+    }
+}
+
+
+
+
+
+
